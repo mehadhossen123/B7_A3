ea6760e60be7074339068332f6509a521288f862
Mehad Hossen <mehad004938@gmail.com>
2026-06-14 15:14:17 +0600

    create database

---
+++ query.sql
@@ -0,0 +1,9 @@
+
+
+-- =========================================================================
+-- SYSTEM: Football Ticket Booking System Database Setup Template
+
+-- =========================================================================
+
+--create database for Ticket store 
+create database ticketBookingSystem
