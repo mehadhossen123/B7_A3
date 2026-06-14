b0c805e4b0788bcddb15e077d01bde512f280404
Mehad Hossen <mehad004938@gmail.com>
2026-06-14 15:43:11 +0600

    create booking table

--- query.sql
+++ query.sql
@@ -27,10 +27,24 @@ CREATE TABLE Users (
 CREATE TABLE Matches (
     match_id serial primary key ,
     fixture varchar(100) not null,
-    tournament_category varchar(100),
+    tournament_category varchar(100) ,
     base_ticket_price decimal(10,2) check (base_ticket_price>0),
     match_status varchar(100) check (match_status in ('Available','Selling Fast','Sold Out','Postponed'))
     
+    
+);
+
+
+-- =========================================================================
+-- 3. CREATE BOOKINGS TABLE
+-- =========================================================================
+CREATE TABLE Bookings (
+    booking_id serial primary key,
+    user_id int not null references users(user_id),
+    match_id int not null references matches(match_id),
+    seat_number int not null,
+    payment_status varchar(50) check (payment_status in('Pending','Confirmed','Cancelled','Refunded')),
+    total_cost decimal(10,2) check (total_cost >=0)
     
 );
 
