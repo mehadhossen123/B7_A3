e3e6486fa7fc06364a5337513c4cc5ca73339bb1
Mehad Hossen <mehad004938@gmail.com>
2026-06-14 22:56:20 +0600

    done problem 6

--- query.sql
+++ query.sql
@@ -121,3 +121,12 @@ select booking_id,user_id,match_id,seat_number,
 select users.user_id,  full_name,booking_id
   from users left join bookings on 
   users.user_id=bookings.user_id;
+
+
+
+  --Query 6: Find all ticket bookings where the total cost is
+ -- strictly higher than the average cost of all ticket bookings.
+
+select avg(total_cost)  from bookings 
+select booking_id,match_id,total_cost from bookings where total_cost > 
+  (select avg(total_cost)  from bookings);
