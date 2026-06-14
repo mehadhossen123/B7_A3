f9fb95b48309aac8c2bceccceee509b15092811d
Mehad Hossen <mehad004938@gmail.com>
2026-06-14 15:15:07 +0600

    create user table

--- query.sql
+++ query.sql
@@ -7,3 +7,14 @@
 
 --create database for Ticket store 
 create database ticketBookingSystem
+
+-- =========================================================================
+-- 1. CREATE USERS TABLE
+-- =========================================================================
+CREATE TABLE Users (
+    user_id serial primary key,
+    full_name varchar(100) not null,
+    email varchar(50) not null unique,
+    role varchar(50) not null,
+    phone_number varchar(11)
+);
